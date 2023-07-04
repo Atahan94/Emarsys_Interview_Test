80e355237348c11230e6df95697837a696438291
Ali Atahan Altınsaban <1905atahan94@gmail.com>
2023-07-04 02:19:44 +0200

    Final Commit

--- Main.js
+++ Main.js
@@ -10,14 +10,13 @@
 
       const startDateObj = new Date(startdate); // Parse start date string into a Date object
 
-      const workingDays = Math.floor(time / 8); // Number of full working days
+      const workingDays = Math.floor(time / 8); // Number of full working days base on given turnaround time 
       const remainingHours = time % 8; // Remaining hours beyond full working days
 
       let dueDate = addWorkingDays(startDateObj, workingDays); // Calculate due date based on working days
-      dueDate = addRemainingHours(dueDate, remainingHours);
+      dueDate = addRemainingHours(dueDate, remainingHours); // Adding remaining hours
 
     
-     // return formatDate(dueDate);
    return formatDate(dueDate);
  }
 
@@ -50,13 +49,13 @@
   return newDate;
 }
 
-function isValidDate(date) 
+function isValidDate(date) // To validate our recived data before our calculation to prevent errors 
 {
   const dateFormat = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$/;
   return dateFormat.test(date);
 }
 
-function formatDate(date) {
+function formatDate(date) { // Formats the date as String Date/Time
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const day = String(date.getDate()).padStart(2, '0');
@@ -65,14 +64,6 @@ function formatDate(date) {
 
   return `${year}-${month}-${day} ${hours}:${minutes}`;
 }
-
-let curDate = new Date();
-const formattedDateTime = curDate.toString();
-
-console.log(CalculateDueDate('2023-07-11 12:30', 16));
-
-
-
 
 
 module.exports = {
